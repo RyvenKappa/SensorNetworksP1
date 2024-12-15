b5f756905d18ba3919162ac1d9f505d6cbae643b
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-15 20:05:30 +0100

    Redefined frame struct to add light and moisture, acceleration changed

--- modules/shared_data/shared_data.h
+++ modules/shared_data/shared_data.h
@@ -8,20 +8,21 @@
 *
 */
 typedef struct PACKED {
-    uint8_t     header;     //1 version and type
-    uint8_t     gps_conn;   //1, 0 or N sats
-    float       latitude;     //4
-    float       longitude;    //4
+    uint8_t     header;    //1 version and type and GPS connection
+    float       latitude;  //4
+    float       longitude; //4
     uint16_t    altitude;  //2
-    int16_t     x_acc;      //2
-    int16_t     y_acc;      //2
-    int16_t     z_acc;      //2
+    int8_t     x_acc;      //2
+    int8_t     y_acc;      //2
+    int8_t     z_acc;      //2
     int16_t     temp;       //2
     int16_t     hum;        //2
     uint16_t    clear;      //2
     uint16_t    red;        //2
     uint16_t    green;      //2
     uint16_t    blue;       //2
+    uint16_t    light;      //2
+    uint16_t    moisture;   //2
 } frame_data_t;
 
 extern frame_data_t frame_data;
