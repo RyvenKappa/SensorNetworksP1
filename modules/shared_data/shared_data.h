47d79567f79a9702877de665ce6534ef664d2f71
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-15 15:27:51 +0100

    Unified shared data to create a frame shared data

--- modules/shared_data/shared_data.h
+++ modules/shared_data/shared_data.h
@@ -1,6 +1,30 @@
 #ifndef SHARED_DATA_H
 #define SHARED_DATA_H
 #include "mbed.h"
+#include <cstdint>
+/**
+* 
+* TODO comentarlo
+*
+*/
+typedef struct PACKED{
+    float latitude;     //4
+    float longitude;    //4
+    uint16_t altitude;  //2
+    int16_t x_acc;      //2
+    int16_t y_acc;      //2
+    int16_t z_acc;      //2
+    int16_t temp;       //2
+    int16_t hum;        //2
+    int16_t clear;      //2
+    int16_t red;        //2
+    int16_t green;      //2
+    int16_t blue;       //2
+}frame_data_t;
+
+extern frame_data_t frame_data;
+
+extern Mutex frame_data_mutex;
 
 /**
 * 
