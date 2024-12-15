a689046c8dc0e939323484950c9e9466826eb3ad
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-15 17:23:16 +0100

    Modified shared to add header and fix types

--- modules/shared_data/shared_data.h
+++ modules/shared_data/shared_data.h
@@ -7,20 +7,22 @@
 * TODO comentarlo
 *
 */
-typedef struct PACKED{
-    float latitude;     //4
-    float longitude;    //4
-    uint16_t altitude;  //2
-    int16_t x_acc;      //2
-    int16_t y_acc;      //2
-    int16_t z_acc;      //2
-    int16_t temp;       //2
-    int16_t hum;        //2
-    int16_t clear;      //2
-    int16_t red;        //2
-    int16_t green;      //2
-    int16_t blue;       //2
-}frame_data_t;
+typedef struct PACKED {
+    uint8_t     header;     //1 version and type
+    uint8_t     gps_conn;   //1, 0 or N sats
+    float       latitude;     //4
+    float       longitude;    //4
+    uint16_t    altitude;  //2
+    int16_t     x_acc;      //2
+    int16_t     y_acc;      //2
+    int16_t     z_acc;      //2
+    int16_t     temp;       //2
+    int16_t     hum;        //2
+    uint16_t    clear;      //2
+    uint16_t    red;        //2
+    uint16_t    green;      //2
+    uint16_t    blue;       //2
+} frame_data_t;
 
 extern frame_data_t frame_data;
 
