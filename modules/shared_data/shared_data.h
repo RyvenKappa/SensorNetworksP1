970c3e24278ec9f5f8f24ad16a3676b325847f41
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-16 20:06:20 +0100

    New frame structure with header, light and moisture, using bitfields

--- modules/shared_data/shared_data.h
+++ modules/shared_data/shared_data.h
@@ -8,21 +8,21 @@
 *
 */
 typedef struct PACKED {
-    uint8_t     header;    //1 version and type and GPS connection
-    float       latitude;  //4
-    float       longitude; //4
-    uint16_t    altitude;  //2
-    int8_t     x_acc;      //2
-    int8_t     y_acc;      //2
-    int8_t     z_acc;      //2
-    int16_t     temp;       //2
-    int16_t     hum;        //2
-    uint16_t    clear;      //2
-    uint16_t    red;        //2
-    uint16_t    green;      //2
-    uint16_t    blue;       //2
-    uint16_t    light;      //2
-    uint16_t    moisture;   //2
+    uint8_t     header    : 8;    //1B version and type and flags TODO
+    uint32_t    latitude  : 32;   //4B full float latitude
+    uint32_t    longitude : 32;   //4B full float longitude
+    uint16_t    altitude  : 16;   //2B full altitude from the gps 
+    uint16_t    clear     : 16;   //2B full clear data from the color sensor
+    uint16_t    red       : 16;   //2B full red data from the color sensor
+    uint16_t    green     : 16;   //2B full green data from the color sensor
+    uint16_t    blue      : 16;   //2B full blue data from the color sensor
+    int16_t     temp      : 16;   //2B full temp data from the temperature sensor
+    uint16_t    hum       : 10;   //10b humidity as a percentage * 10
+    int16_t     x_acc     : 14;   //14b acceleration in the x axis from the sensor
+    int16_t     y_acc     : 14;   //14b acceleration in the y axis from the sensor 
+    int16_t     z_acc     : 14;   //14b acceleration in the z axis from the sensor
+    uint16_t    light     : 10;   //10b light as a percentage * 10
+    uint16_t    moisture  : 10;   //10b moisture as a percentage * 10
 } frame_data_t;
 
 extern frame_data_t frame_data;
