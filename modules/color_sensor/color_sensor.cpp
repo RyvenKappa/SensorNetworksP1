e7facf200768855dfa8d143994d6840e78b39d29
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-16 22:45:10 +0100

    Color sensor refactorized

--- modules/color_sensor/color_sensor.cpp
+++ modules/color_sensor/color_sensor.cpp
@@ -14,11 +14,12 @@ static char data[8];
 * Static function for measurement processing
 */
 static void measurement_process(){
+    // Original data is processed and inserted in the frame structure for lora
     frame_data_mutex.lock();
     frame_data.clear = data[1]<<8 | data[0];
     frame_data.red = data[3]<<8 | data[2];
-    frame_data.green = (data[5]<<8 | data[4]) + frame_data.red*0.15;
-    frame_data.blue = (data[7]<<8 | data[6]) + frame_data.red*0.25;
+    frame_data.green = (data[5]<<8 | data[4]) + frame_data.red*0.15; //Spectral compensation is added to the green.
+    frame_data.blue = (data[7]<<8 | data[6]) + frame_data.red*0.25;  //Spectral compensation is added to the blue.
     frame_data_mutex.unlock();
 }
 
@@ -45,11 +46,13 @@ void color_sensor_read(){
 
 
 void color_sensor_init(){
-    //color_int.rise(&color_sensor_handler);
+    // Init configuration: Wait dissabled, interruptions dissabled and AEN deactivated.
     cmd[0] = COLOR_SENSOR_CMD_MSK | COLOR_SENSOR_TYPE_AUTO_INCREMENT | COLOR_SENSOR_ENABLE;
-    cmd[1] = 0x02; //Wait dissabled, interruptions dissabled and AEN deactivated
+    cmd[1] = 0x02; 
     i2c_bus.write(COLOR_SENSOR_SLAVE_ADDRESS<<1,cmd,2);
+
+    // Init configuration: 154ms of RGBC Integration
     cmd[0] = COLOR_SENSOR_CMD_MSK | COLOR_SENSOR_TYPE_AUTO_INCREMENT | COLOR_SENSOR_ATIME;
-    cmd[1] = 0xC0;//154ms of RGBC Integration
+    cmd[1] = 0xC0;
     i2c_bus.write(COLOR_SENSOR_SLAVE_ADDRESS<<1,cmd,2);
 }
--- modules/color_sensor/color_sensor.h
+++ modules/color_sensor/color_sensor.h
@@ -18,40 +18,32 @@
 
 #define COLOR_SENSOR_INT PA_11
 
-#define COLOR_SENSOR_SLAVE_ADDRESS 0x29
+#define COLOR_SENSOR_SLAVE_ADDRESS       0x29
 
-#define COLOR_SENSOR_CMD_MSK 0x80
+#define COLOR_SENSOR_CMD_MSK             0x80
 
 #define COLOR_SENSOR_TYPE_AUTO_INCREMENT 0x20 //Working interest for this project
 
-#define COLOR_SENSOR_ENABLE 0x00
-#define COLOR_SENSOR_ATIME 0x01
-#define COLOR_SENSOR_WTIME 0x03
-#define COLOR_SENSOR_AILTL 0x04
-#define COLOR_SENSOR_AILTH 0x05
-#define COLOR_SENSOR_AIHTL 0x06
-#define COLOR_SENSOR_AIHTH 0x07
-#define COLOR_SENSOR_PERS 0x0C
-#define COLOR_SENSOR_CONFIG 0x0D
-#define COLOR_SENSOR_CONTROL 0x0F
-#define COLOR_SENSOR_ID 0x12
-#define COLOR_SENSOR_STATUS 0x13
-#define COLOR_SENSOR_CDATAL 0x14
-#define COLOR_SENSOR_CDATAH 0x15
-#define COLOR_SENSOR_RDATAL 0x16
-#define COLOR_SENSOR_RDATAH 0x17
-#define COLOR_SENSOR_GDATAL 0x18
-#define COLOR_SENSOR_GDATAH 0x19
-#define COLOR_SENSOR_BDATAL 0x1A
-#define COLOR_SENSOR_BDATAH 0x1B
-
-
-extern volatile int color_sensor_message;
-/**
-* @brief Cleanning and reading of Interrupt in the color sensor
-* @param read If true, reads the value of the clear data in the interrupt register and sends a msg, if false, only interruption cleaning.
-*/
-extern void clean_interrupt(bool read);
+#define COLOR_SENSOR_ENABLE              0x00
+#define COLOR_SENSOR_ATIME               0x01
+#define COLOR_SENSOR_WTIME               0x03
+#define COLOR_SENSOR_AILTL               0x04
+#define COLOR_SENSOR_AILTH               0x05
+#define COLOR_SENSOR_AIHTL               0x06
+#define COLOR_SENSOR_AIHTH               0x07
+#define COLOR_SENSOR_PERS                0x0C
+#define COLOR_SENSOR_CONFIG              0x0D
+#define COLOR_SENSOR_CONTROL             0x0F
+#define COLOR_SENSOR_ID                  0x12
+#define COLOR_SENSOR_STATUS              0x13
+#define COLOR_SENSOR_CDATAL              0x14
+#define COLOR_SENSOR_CDATAH              0x15
+#define COLOR_SENSOR_RDATAL              0x16
+#define COLOR_SENSOR_RDATAH              0x17
+#define COLOR_SENSOR_GDATAL              0x18
+#define COLOR_SENSOR_GDATAH              0x19
+#define COLOR_SENSOR_BDATAL              0x1A
+#define COLOR_SENSOR_BDATAH              0x1B
 
 /**
 * @brief Reading of the color data.
