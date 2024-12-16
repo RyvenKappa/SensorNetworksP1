910f3eecb271f474e9d9d2e2789890566081fe0e
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-16 22:45:45 +0100

    Brightness sensor refactorized

--- modules/brightness_sensor/brightness_sensor.cpp
+++ modules/brightness_sensor/brightness_sensor.cpp
@@ -5,10 +5,6 @@
 
 static AnalogIn value_reader(BRIGHTNESS_READ_PIN);
 
-
-/**
-* Static function for the thread loop
-*/
 float read_brightness_sensor_data(){
     return ((float)value_reader.read_u16()*100)/0xFFFF;
 }
