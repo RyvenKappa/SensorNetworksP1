566568519fc6851c1171fcfa1ad66891e430248a
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-16 22:59:50 +0100

    Shared data commented

--- modules/shared_data/shared_data.h
+++ modules/shared_data/shared_data.h
@@ -3,26 +3,36 @@
 #include "mbed.h"
 #include <cstdint>
 /**
-* 
-* TODO comentarlo
+* @file shared_data.h
+*
+* @brief Shared data module to define the structures that are sent to the lorawan modules.
+*
+* @author Diego Aceituno Seoane
+*
+* @date 16-12-2024
+* Controls a Si7021-A20 temperature and Humidity sensor
 *
 */
+
+/**
+* @brief Structure for the frame sent to the lorawan application
+*/
 typedef struct PACKED {
-    uint8_t     header    : 8;    //1B version and type and flags TODO
-    uint32_t    latitude  : 32;   //4B full float latitude
-    uint32_t    longitude : 32;   //4B full float longitude
-    uint16_t    altitude  : 16;   //2B full altitude from the gps 
-    uint16_t    clear     : 16;   //2B full clear data from the color sensor
-    uint16_t    red       : 16;   //2B full red data from the color sensor
-    uint16_t    green     : 16;   //2B full green data from the color sensor
-    uint16_t    blue      : 16;   //2B full blue data from the color sensor
-    int16_t     temp      : 16;   //2B full temp data from the temperature sensor
-    uint16_t    hum       : 10;   //10b humidity as a percentage * 10
-    int16_t     x_acc     : 14;   //14b acceleration in the x axis from the sensor
-    int16_t     y_acc     : 14;   //14b acceleration in the y axis from the sensor 
-    int16_t     z_acc     : 14;   //14b acceleration in the z axis from the sensor
-    uint16_t    light     : 10;   //10b light as a percentage * 10
-    uint16_t    moisture  : 10;   //10b moisture as a percentage * 10
+    uint8_t     header    : 8;    /*!< 1B version and type and flags */
+    uint32_t    latitude  : 32;   /*!< 4B full float latitude */
+    uint32_t    longitude : 32;   /*!< 4B full float longitude */
+    uint16_t    altitude  : 16;   /*!< 2B full altitude from the gps */
+    uint16_t    clear     : 16;   /*!< 2B full clear data from the color sensor */
+    uint16_t    red       : 16;   /*!< 2B full red data from the color sensor */
+    uint16_t    green     : 16;   /*!< 2B full green data from the color sensor */
+    uint16_t    blue      : 16;   /*!< 2B full blue data from the color sensor */
+    int16_t     temp      : 16;   /*!< 2B full temp data from the temperature sensor */
+    uint16_t    hum       : 10;   /*!< 10b humidity as a percentage with 1 digit * 10 */
+    int16_t     x_acc     : 14;   /*!< 14b acceleration in the x axis from the sensor */
+    int16_t     y_acc     : 14;   /*!< 14b acceleration in the y axis from the sensor */
+    int16_t     z_acc     : 14;   /*!< 14b acceleration in the z axis from the sensor */
+    uint16_t    light     : 10;   /*!< 10b light as a percentage with 1 digit * 10 */
+    uint16_t    moisture  : 10;   /*!< 10b moisture as a percentage with 1 digit * 10 */
 } frame_data_t;
 
 extern frame_data_t frame_data;
