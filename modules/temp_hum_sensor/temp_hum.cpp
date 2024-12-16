a0d54c5c067d3978a39c326eb0720c0785dd51fe
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-16 22:20:06 +0100

    Temperature and humidity module refactored

--- modules/temp_hum_sensor/temp_hum.cpp
+++ modules/temp_hum_sensor/temp_hum.cpp
@@ -9,32 +9,29 @@ static char cmd;
 static char buff[2];
 
 /**
-* Reading function with conversion
+* Static reading function for the values.
 */
 static void converted_reading(){
+    // Read sensor's humidity
     cmd = TH_SENSOR_MEASUREH_MASTER;
     i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1,true);
-    //Humidity
     i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
-    //conversion
+
+    // Perform the conversion of the value and store in the frame data for the next message.
     frame_data_mutex.lock();
     frame_data.hum = (uint16_t)(((125.0*(buff[0]<<8 | buff[1])/65536.0)-6)*10);
     frame_data_mutex.unlock();
 
-
-    //Obtain last temperature measurement
+    //Obtain last temperature measurement from the sensor
     cmd = TH_SENSOR_MEASURET_PREVIOUS;
     i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1);
     i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
-    //Conversion
+    // Store the 16 bit value in the frame data for the next message without conversion.
     frame_data_mutex.lock();
     frame_data.temp = (buff[0]<<8 | buff[1]);
     frame_data_mutex.unlock();
 }
 
-/**
-* Static function for the thread loop
-*/
 void temp_hum_sensor_read(){
     converted_reading();
 }
