82a4f4179ee11b86e9ac0c243844be1e74b5a490
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-12-17 22:36:10 +0100

    Moisture using debug macro defined in compile time

--- modules/moisture_sensor/moisture_sensor.cpp
+++ modules/moisture_sensor/moisture_sensor.cpp
@@ -4,15 +4,23 @@
 #define MOISTURE_POWER_PIN PA_13
 
 static AnalogIn value_reader(MOISTURE_READ_PIN);
+#ifdef DEBUG_MODE
 static DigitalOut power_pin(MOISTURE_POWER_PIN);
+#endif
 
 /**
 * Static function for the thread loop
 */
 float read_moisture_sensor_data(){
+    #ifdef DEBUG_MODE
     power_pin.write(1);
     ThisThread::sleep_for(1ms);
+    #endif
+
     float return_value = ((float)value_reader.read_u16()*100)/0xFFFF;
+    
+    #ifdef DEBUG_MODE
     power_pin.write(0);
+    #endif
     return return_value;
 }
